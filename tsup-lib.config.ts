622edc0a8fc3afa21515fc4eaedf4f7f7b5ea1e4
Rishabh Gupta <38923768+imrishabh18@users.noreply.github.com>
2025-04-30 03:54:02 +0530

    Bundle `parts-engine` (#251)
    * Bundle `parts-engine`
    
    * format

---
+++ tsup-lib.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from "tsup"
+
+export default defineConfig({
+  entry: ["./lib/index.ts"],
+  format: ["esm"],
+  sourcemap: "inline",
+  outDir: "dist/lib",
+  noExternal: ["@tscircuit/parts-engine"],
+  clean: true,
+  dts: true,
+})
