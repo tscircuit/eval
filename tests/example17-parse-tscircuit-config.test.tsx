bb2dfa1debdc392235504be9babf4ba062d2caab
Rishabh Gupta <38923768+imrishabh18@users.noreply.github.com>
2025-06-19 22:54:43 +0530

    set the config file to the `opts.entrypoint` (#537)

---
+++ tests/example17-parse-tscircuit-config.test.tsx
@@ -0,0 +1,49 @@
+import { createCircuitWebWorker } from "lib"
+import { expect, test } from "bun:test"
+
+test("parse tscircuit.config.js with mainEntrypoint", async () => {
+  const circuitWebWorker = await createCircuitWebWorker({
+    webWorkerUrl: new URL("../webworker/entrypoint.ts", import.meta.url),
+  })
+
+  await circuitWebWorker.executeWithFsMap({
+    fsMap: {
+      "tscircuit.config.json": `
+        {
+          "mainEntrypoint": "random1.tsx"
+        }
+      `,
+      "random1.tsx": `
+        import { MyLed } from "./random2.tsx"
+        import someJson from "./random3.json"
+        
+        circuit.add(
+          <board width="10mm" height="10mm">
+            <MyLed name="LED1" />
+          </board>
+        )
+      `,
+      "random2.tsx": `
+        import { RedLed } from "@tsci/seveibar.red-led"
+        
+        export const MyLed = ({ name }) => {
+          return <RedLed name={name} />
+        }
+      `,
+      "random3.json": `
+        {
+          "some": "value"
+        }
+      `,
+    },
+  })
+
+  await circuitWebWorker.renderUntilSettled()
+
+  const circuitJson = await circuitWebWorker.getCircuitJson()
+
+  const led = circuitJson.find((el: any) => el.name === "LED1")
+  expect(led?.type).toBe("source_component")
+
+  await circuitWebWorker.kill()
+})
