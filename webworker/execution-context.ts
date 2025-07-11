90b7d8ccd3be7807cd5ee59c3c9617a6ae89f9a2
seveibar <seveibar@gmail.com>
2025-07-10 17:16:46 -0700

    add react version

--- webworker/execution-context.ts
+++ webworker/execution-context.ts
@@ -24,6 +24,9 @@ export function createExecutionContext(
     platform?: PlatformConfig
   } = {},
 ): ExecutionContext {
+  debug("createExecutionContext", {
+    ReactVersion: React.version,
+  })
   globalThis.React = React
 
   const circuit = new RootCircuit({
