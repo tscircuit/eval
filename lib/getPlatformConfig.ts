06a537e9a9a42dd6103bea61e7eaa831a59366d5
imrishabh18 <belikerishabh@gmail.com>
2025-09-08 16:29:43 +0530

    add kicad-mod-server in eval

--- lib/getPlatformConfig.ts
+++ lib/getPlatformConfig.ts
@@ -1,6 +1,15 @@
 import type { PlatformConfig } from "@tscircuit/props"
 import { jlcPartsEngine } from "@tscircuit/parts-engine"
 
+const KICAD_FOOTPRINT_CACHE_URL = "https://kicad-mod-cache.tscircuit.com/"
+
 export const getPlatformConfig = (): PlatformConfig => ({
   partsEngine: jlcPartsEngine,
+  footprintLibraryMap: {
+    kicad: async (footprintName: string) => {
+      const url = `${KICAD_FOOTPRINT_CACHE_URL}/${footprintName}.circuit.json`
+      const res = await fetch(url)
+      return { footprintCircuitJson: await res.json() }
+    },
+  },
 })
---
+++ tests/examples/example18-kicad-footprint-server.test.tsx
@@ -0,0 +1,36 @@
+import { createCircuitWebWorker } from "lib/index"
+import { expect, test } from "bun:test"
+
+test("example18-kicad-footprint-server", async () => {
+  const circuitWebWorker = await createCircuitWebWorker({
+    webWorkerUrl: new URL("../../webworker/entrypoint.ts", import.meta.url),
+    verbose: true,
+  })
+
+  await circuitWebWorker.executeWithFsMap({
+    entrypoint: "index.tsx",
+    fsMap: {
+      "index.tsx": `
+          circuit.add(
+            <board>
+              <resistor name="R1" resistance="1k" footprint="kicad:Resistor_SMD.pretty/R_0402_1005Metric" pcbX={-2} />
+              <capacitor name="C1" capacitance="100uF" footprint="0402" pcbX={2} />
+              <trace from=".R1 > .pin2" to=".C1 > .pin1" />
+            </board>
+          )
+        `,
+    },
+  })
+
+  await circuitWebWorker.renderUntilSettled()
+
+  const circuitJson = await circuitWebWorker.getCircuitJson()
+
+  Bun.write("circuit.json", JSON.stringify(circuitJson, null, 2))
+
+  const pcb_trace = circuitJson.filter((el: any) => el.type === "pcb_trace")
+  expect(pcb_trace).toBeDefined()
+  expect(pcb_trace.length).toBe(1)
+
+  await circuitWebWorker.kill()
+})
