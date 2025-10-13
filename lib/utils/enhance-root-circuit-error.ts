942fff56cd239c54607c573b4711e14f4e09f13a
Severin Ibarluzea <seveibar@gmail.com>
2025-10-13 16:14:26 -0700

    Improve entrypoint empty circuit error message (#1262)

---
+++ lib/utils/enhance-root-circuit-error.ts
@@ -0,0 +1,18 @@
+export const enhanceRootCircuitHasNoChildrenError = (
+  error: unknown,
+  entrypoint?: string,
+) => {
+  if (
+    error instanceof Error &&
+    entrypoint &&
+    error.message.includes("RootCircuit has no children") &&
+    !error.message.includes('"entrypoint":')
+  ) {
+    const entrypointMessage = entrypoint.startsWith("./")
+      ? entrypoint.slice(2)
+      : entrypoint
+    error.message = `${error.message}. "entrypoint": "${entrypointMessage}" is set in the runner configuration, entrypoints must contain "circuit.add(...)", you might be looking to use mainComponentPath instead if your file exports a component.`
+  }
+
+  return error
+}
