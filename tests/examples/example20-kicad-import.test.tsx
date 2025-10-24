02d66467c624150654ce35fe44517ca8638d2e5b
Arnav K <arnavkaushal09@gmail.com>
2025-10-25 03:45:13 +0530

    f

--- tests/examples/example20-kicad-import.test.tsx
+++ tests/examples/example20-kicad-import.test.tsx
@@ -1,7 +1,6 @@
 import { CircuitRunner, getPlatformConfig } from "lib/index"
 import { test } from "bun:test"
 import { expect } from "bun:test"
-import { createCircuitWebWorker } from "lib/worker"
 
 test(
   "example20-kicad-direct-import",
@@ -43,55 +42,6 @@ test(
 
     await runner.renderUntilSettled()
     await runner.kill()
-  },
-  20 * 1000,
-)
-
-test(
-  "example20-kicad-direct-import-via-webworker",
-  async () => {
-    const circuitWebWorker = await createCircuitWebWorker({
-      webWorkerUrl: new URL("../../webworker/entrypoint.ts", import.meta.url),
-      platform: {
-        ...getPlatformConfig(),
-        footprintFileParserMap: {
-          kicad_mod: {
-            loadFromUrl: async (url: string) => {
-              console.log("loadFromUrl", url)
-              return {
-                footprintCircuitJson: [],
-              }
-            },
-          },
-        },
-      },
-    })
-
-    try {
-      await circuitWebWorker.executeWithFsMap({
-        fsMap: {
-          "index.tsx": `
-			import kicadMod from "./poly.kicad_mod"
-
-			export default () => {
-			  return (
-				<board width="10mm" height="10mm">
-				  <chip
-					name="U1"
-					footprint={kicadMod}
-				  />
-				</board>
-			  )
-			}
-			`,
-          "poly.kicad_mod": "__STATIC_ASSET__",
-        },
-      })
-
-      await circuitWebWorker.renderUntilSettled()
-    } finally {
-      await circuitWebWorker.kill()
-    }
   },
   20 * 1000,
 )
---
+++ tests/repros/platform-config-not-being-overriden.test.tsx
@@ -0,0 +1,56 @@
+import { getPlatformConfig } from "lib/index"
+import { test } from "bun:test"
+import { createCircuitWebWorker } from "lib/worker"
+
+test(
+  "platform-config-not-being-overriden",
+  async () => {
+    const circuitWebWorker = await createCircuitWebWorker({
+      webWorkerUrl: new URL("../../webworker/entrypoint.ts", import.meta.url),
+      platform: {
+        ...getPlatformConfig(),
+        footprintFileParserMap: {import { CircuitRunner, getPlatformConfig } from "lib/index"
+        import { test } from "bun:test"
+        import { expect } from "bun:test"
+        import { createCircuitWebWorker } from "lib/worker"
+        
+          kicad_mod: {
+            loadFromUrl: async (url: string) => {
+              console.log("loadFromUrl", url)
+              return {
+                footprintCircuitJson: [],
+              }
+            },
+          },
+        },
+      },
+    })
+
+    try {
+      await circuitWebWorker.executeWithFsMap({
+        fsMap: {
+          "index.tsx": `
+              import kicadMod from "./poly.kicad_mod"
+  
+              export default () => {
+                return (
+                  <board width="10mm" height="10mm">
+                    <chip
+                      name="U1"
+                      footprint={kicadMod}
+                    />
+                  </board>
+                )
+              }
+              `,
+          "poly.kicad_mod": "__STATIC_ASSET__",
+        },
+      })
+
+      await circuitWebWorker.renderUntilSettled()
+    } finally {
+      await circuitWebWorker.kill()
+    }
+  },
+  20 * 1000,
+)
