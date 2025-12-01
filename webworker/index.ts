c1b753d14971a66e3305f57ee6e1be91ec62c35f
seveibar <seveibar@gmail.com>
2025-11-30 18:28:21 -0800

    minor refactor

--- webworker/index.ts
+++ webworker/index.ts
@@ -2,11 +2,3 @@
  * Webworker entrypoint and re-exports from lib/eval
  */
 export * from "./entrypoint"
-export {
-  extractBasePackageName,
-  isPackageDeclaredInPackageJson,
-  getNodeModuleDirectory,
-  getPackageJsonEntrypoint,
-  isTypeScriptEntrypoint,
-  isDistDirEmpty,
-} from "lib/eval"
