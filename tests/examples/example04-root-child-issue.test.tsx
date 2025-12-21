0787a46c3a6688fe10963c5df897d7483a0f90b2
imrishabh18 <belikerishabh@gmail.com>
2025-12-21 17:11:25 +0530

    fix test

--- tests/examples/example04-root-child-issue.test.tsx
+++ tests/examples/example04-root-child-issue.test.tsx
@@ -7,7 +7,7 @@ const example4 = {
     "entrypoint.tsx":
       '\nimport MyCircuit from "./snippet.tsx"\n\ncircuit.add(<MyCircuit />)\n\nconsole.log("rootComponent", circuit._guessRootComponent(), circuit.firstChild)\n',
     "snippet.tsx":
-      'import "@tscircuit/core"\nimport { useRedLed } from "@tsci/seveibar.red-led"\nimport { usePushButton } from "@tsci/seveibar.push-button"\nimport { useUsbC } from "@tsci/seveibar.smd-usb-c"\n\nexport default () => {\n  const USBC = useUsbC("USBC")\n  const Button = usePushButton("SW1")\n  const Led = useRedLed("LED")\n  return (\n    <board width="12mm" height="30mm" schAutoLayoutEnabled>\n      <USBC GND="net.GND" pcbY={-10} VBUS1="net.VBUS" />\n      <Led neg="net.GND" pcbY={12} />\n      <Button pcbY={0} pin2=".R1 > .pos" pin3="net.VBUS" />\n      <resistor name="R1" footprint="0603" resistance="1k" pcbY={7} />\n      <trace from=".R1 > .neg" to={Led.pos} />\n    </board>\n  )\n}\n',
+      'import "@tscircuit/core"\nimport { useRedLed } from "@tsci/seveibar.red-led"\nimport { PushButton } from "@tsci/seveibar.push-button"\nimport { useUsbC } from "@tsci/seveibar.smd-usb-c"\n\nexport default () => {\n  const USBC = useUsbC("USBC")\n  const Led = useRedLed("LED")\n  return (\n    <board width="12mm" height="30mm" schAutoLayoutEnabled>\n      <USBC GND="net.GND" pcbY={-10} VBUS1="net.VBUS" />\n      <Led neg="net.GND" pcbY={12} />\n      <PushButto pcbY={0} pin2=".R1 > .pos" pin3="net.VBUS" />\n      <resistor name="R1" footprint="0603" resistance="1k" pcbY={7} />\n      <trace from=".R1 > .neg" to={Led.pos} />\n    </board>\n  )\n}\n',
   },
 }
 
