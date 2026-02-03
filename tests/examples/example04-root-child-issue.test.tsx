935c5efe6a0e795ab0c3dd7ecd828f88937e507e
imrishabh18 <belikerishabh@gmail.com>
2026-02-03 18:25:22 +0530

    fix test

--- tests/examples/example04-root-child-issue.test.tsx
+++ tests/examples/example04-root-child-issue.test.tsx
@@ -7,7 +7,7 @@ const example4 = {
     "entrypoint.tsx":
       '\nimport MyCircuit from "./snippet.tsx"\n\ncircuit.add(<MyCircuit />)\n\nconsole.log("rootComponent", circuit._guessRootComponent(), circuit.firstChild)\n',
     "snippet.tsx":
-      'import "@tscircuit/core"\nimport { useRedLed } from "@tsci/seveibar.red-led"\nimport { PushButton } from "@tsci/seveibar.push-button"\nimport { useUsbC } from "@tsci/seveibar.smd-usb-c"\n\nexport default () => {\n  const USBC = useUsbC("USBC")\n  const Led = useRedLed("LED")\n  return (\n    <board width="12mm" height="30mm" schAutoLayoutEnabled>\n      <USBC GND="net.GND" pcbY={-10} VBUS1="net.VBUS" />\n      <Led neg="net.GND" pcbY={12} />\n      <PushButton pcbY={0} pin2=".R1 > .pos" pin3="net.VBUS" />\n      <resistor name="R1" footprint="0603" resistance="1k" pcbY={7} />\n      <trace from=".R1 > .neg" to={Led.pos} />\n    </board>\n  )\n}\n',
+      'import "@tscircuit/core"\nimport { RedLed } from "@tsci/seveibar.red-led"\nimport { PushButton } from "@tsci/seveibar.push-button"\nimport { SmdUsbC } from "@tsci/seveibar.smd-usb-c"\n\nexport default () => {\n  return (\n    <board width="12mm" height="30mm" schAutoLayoutEnabled>\n      <SmdUsbC GND="net.GND" pcbY={-10} VBUS1="net.VBUS" />\n      <RedLed name="Led" neg="net.GND" pcbY={12} />\n      <PushButton pcbY={0} pin2=".R1 > .pos" pin3="net.VBUS" />\n      <resistor name="R1" footprint="0603" resistance="1k" pcbY={7} />\n      <trace from=".R1 > .neg" to={RedLed.pos} />\n    </board>\n  )\n}\n',
   },
 }
 
@@ -27,7 +27,7 @@ test(
 
     const circuitJson = await circuitWebWorker.getCircuitJson()
 
-    const led = circuitJson.find((el: any) => el.name === "LED")
+    const led = circuitJson.find((el: any) => el.name === "Led")
     expect(led).toBeDefined()
     expect(led?.type).toBe("source_component")
 
